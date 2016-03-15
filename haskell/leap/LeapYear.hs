750b3939be334d57c11546d1388b714419a12d28
Filipe Azevedo <filipe@azevedo.io>
2016-03-15 23:56:25 +0000

    Leap improvement

--- haskell/leap/LeapYear.hs
+++ haskell/leap/LeapYear.hs
@@ -2,5 +2,7 @@ module LeapYear (isLeapYear) where
 
 isLeapYear :: Int -> Bool
 isLeapYear year
-            | mod year 4 == 0 && mod year 100 /= 0 || mod year 400 == 0 = True
+            | mod year 4 /= 0 = False
+            | mod year 100 /= 0 = True
+            | mod year 400 == 0 = True
             | otherwise = False
