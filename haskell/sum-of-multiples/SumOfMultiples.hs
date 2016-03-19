927c5592dad5dd876e1a1a16e2dae348781189cc
Filipe Azevedo <filipe@azevedo.io>
2016-03-19 12:53:15 +0000

    improvements

--- haskell/sum-of-multiples/SumOfMultiples.hs
+++ haskell/sum-of-multiples/SumOfMultiples.hs
@@ -7,8 +7,8 @@ sumOfMultiples numbers n = sum $ map (sumIfMultiple numbers) $ take (n-1) [1..]
 
 sumIfMultiple :: [Int] -> Int -> Int
 sumIfMultiple numbers n
-        | filter (\x -> mod n x == 0) numbers == [] = 0
-        | otherwise = n
+        | any (\x -> mod n x == 0) numbers = n
+        | otherwise = 0
 
 
 sumOfMultiplesDefault :: Int -> Int
