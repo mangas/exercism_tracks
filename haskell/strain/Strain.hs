dad8a35d63732597b5253228f876db342010f031
Filipe Azevedo <filipe@azevedo.io>
2016-03-19 21:36:44 +0000

    no cheating

--- haskell/strain/Strain.hs
+++ haskell/strain/Strain.hs
@@ -1,7 +1,10 @@
 module Strain (keep, discard) where
 
 keep :: (a -> Bool) -> [a] -> [a]
-keep = filter
+keep predicate [] = []
+keep predicate (x:xs)
+        | predicate x = x:keep predicate xs
+        | otherwise = keep predicate xs
 
 discard :: (a -> Bool) -> [a] -> [a]
 discard predicate = keep (not . predicate)
