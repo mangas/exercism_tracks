e24c5c55fafef8215f10a6c599d4e5b00102a464
Filipe Azevedo <filipe.azevedo@bgch.co.uk>
2016-03-21 16:54:33 +0000

    refactor DNA

--- haskell/nucleotide-count/DNA.hs
+++ haskell/nucleotide-count/DNA.hs
@@ -1,39 +1,33 @@
 module DNA (count, nucleotideCounts) where
 
 import Data.Map.Strict(fromList, Map, (!), updateWithKey)
-import Debug.Trace
+import Data.List
 
-isValid :: Char -> Bool
-isValid 'A' = True
-isValid 'T' = True
-isValid 'C' = True
-isValid 'G' = True
-isValid  _ = False
+valid :: String
+valid = "ATCG"
+
+isValid :: Char -> Char
+isValid c
+        | elem c valid = c
+        | otherwise = error $ "invalid nucleotide " ++ show c
+
 
 emptyMap :: Map Char Int
-emptyMap = fromList [ ('A',0), ('T',0), ('C', 0), ('G', 0) ]
-
-errorMessage :: Char -> String
-errorMessage c = "invalid nucleotide " ++ show c
+emptyMap = fromList $ map (\x -> (x, 0) ) valid
 
 f :: Char -> Int -> Maybe Int
 f key = Just . (+1)
 
 verify :: Char -> Char -> Bool
-verify c x
-        | not $ isValid x = error $ errorMessage x
-        | c == x = True
-        | otherwise = False
+verify c x = c == isValid x
 
 count :: Char -> String -> Int
-count c dna
-        | not $ isValid c = error $ errorMessage c
-        | otherwise = sum $ map (fromEnum . verify c ) dna
+count c dna =
+        let validC = isValid c in
+        sum $ map (fromEnum . verify validC ) dna
 
 nucleotideCounts :: String -> Map Char Int
 nucleotideCounts = foldl doUpdate emptyMap
 
 doUpdate :: Map Char Int -> Char -> Map Char Int
-doUpdate count c
-            | not $ isValid c = error $ errorMessage c
-            | otherwise = updateWithKey f c count
+doUpdate count c = updateWithKey f (isValid c) count
