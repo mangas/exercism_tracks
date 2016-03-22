12de3dee7f405594856e718dee0e4c23262a80c7
Filipe Azevedo <filipe@azevedo.io>
2016-03-22 14:39:03 +0000

    phone number exercise

---
+++ haskell/phone-number/Phone.hs
@@ -0,0 +1,26 @@
+module Phone (areaCode, number, prettyPrint) where
+
+import Data.Char
+import Data.List
+
+areaCode :: String -> String
+areaCode = take 3 . number
+
+number :: String -> String
+number n
+        | size == 11 && (head cleanNumber == '1') = drop 1 cleanNumber
+        | size /= 10 = intercalate "" $ replicate 10 "0"
+        | otherwise = cleanNumber
+         where
+            cleanNumber = filter isDigit n
+            size = length cleanNumber
+
+
+
+prettyPrint :: String -> String
+prettyPrint n =
+        let cleanNumber = number n
+            prefix = "(" ++ ( take 3 cleanNumber ) ++ ") "
+            fstNum = (drop 3 $ take 6 cleanNumber)  ++ "-"
+            sndNum = drop 6 cleanNumber
+        in prefix ++ fstNum ++ sndNum
