78b74d2e3973e5fcfe7b0ba0220b5bf2ab29925e
Filipe Azevedo <filipe@azevedo.io>
2016-03-27 22:36:28 +0100

    hlint

--- haskell/sum-of-multiples/SumOfMultiples.hs
+++ haskell/sum-of-multiples/SumOfMultiples.hs
@@ -2,7 +2,7 @@ module SumOfMultiples (sumOfMultiples, sumOfMultiplesDefault) where
 
 
 sumOfMultiples :: [Int] -> Int -> Int
-sumOfMultiples numbers n = sum $ filter (isMultiple) $ take (n-1) [1..]
+sumOfMultiples numbers n = sum $ filter isMultiple $ take (n-1) [1..]
                         where isMultiple y = any ((== 0) . mod y) numbers
 
 
