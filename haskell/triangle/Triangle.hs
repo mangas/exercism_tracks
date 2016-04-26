2139027914d2263f29307081df1f919fad2af947
Filipe Azevedo <filipe@azevedo.io>
2016-04-26 18:38:34 +0100

    triangle exercise improvement

--- haskell/triangle/Triangle.hs
+++ haskell/triangle/Triangle.hs
@@ -7,7 +7,7 @@ data TriangleType = Equilateral | Isosceles | Scalene | Illogical
 
 triangleType :: Int -> Int -> Int -> TriangleType
 triangleType s1 s2 s3 
-            | any (==0) ss = Illogical
+            | elem 0 ss = Illogical
             | s1+s2<=s3 || s2+s3<=s1 || s1+s3<=s2 = Illogical
             | otherwise =  toEnum $ length uniq -1 ::TriangleType 
             where 
