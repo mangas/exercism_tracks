5ea19d1419523ddac24cb120d40c02529847e4fa
Filipe Azevedo <filipe@azevedo.io>
2016-04-26 11:20:34 +0100

    improvement

--- haskell/triangle/Triangle.hs
+++ haskell/triangle/Triangle.hs
@@ -3,15 +3,13 @@ module Triangle (TriangleType(..), triangleType) where
 import Data.List
    
 data TriangleType = Equilateral | Isosceles | Scalene | Illogical
-    deriving (Show, Eq)
+    deriving (Enum, Show, Eq)
 
 triangleType :: Int -> Int -> Int -> TriangleType
 triangleType s1 s2 s3 
             | any (==0) ss = Illogical
-            | s1+s2<=s3 || s2+s3<=s1 || s1+s3<=s2 = Illogical 
-            | length uniq == 1 = Equilateral
-            | length uniq == 2 = Isosceles
-            | length uniq == 3 = Scalene 
+            | s1+s2<=s3 || s2+s3<=s1 || s1+s3<=s2 = Illogical
+            | otherwise =  toEnum $ length uniq -1 ::TriangleType 
             where 
                 ss = [s1, s2, s3]
                 uniq = map head $ group $ sort ss
