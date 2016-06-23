c0bc92af7b98a03dc737f9c9279a7b946f404663
Filipe Azevedo <filipe@azevedo.io>
2016-06-23 18:40:25 +0100

    Scala Bob complete exercise

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,14 +1,13 @@
+/**
+  * Created by fa on 6/17/16.
+  */
 class Bob {
 
-  def hey(message: String): String = {
-    message match {
-      case msg: String => {
-          case x if isShouting(x) => "Whoa, chill out!"
-          case x if isStatement(x) => "respond to a statement"
-          case x if isQuestion(x) => "Sure."
-          case x if isSilence(x) => "Fine. Be that way!"
-      }
-      case _ => return null
-    }
+  def hey(msg: String): String = msg match {
+    case x if x.filterNot( _ == ' ').length == 0 => "Fine. Be that way!"
+    case x if x.filter(_.isLetter).length == 0 => if (x.endsWith("?")) "Sure." else "Whatever."
+    case x if x.filter(_.isLetter).forall(_.isUpper)  => "Whoa, chill out!"
+    case x if x.endsWith("?") => "Sure."
+    case _ => "Whatever."
   }
 }
