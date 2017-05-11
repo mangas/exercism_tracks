20dbcdafcac7b35f1f6cdbb283dbe94484905f68
Filipe Azevedo <filipe@azevedo.io>
2017-05-11 23:23:57 +0100

    length-encoding

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -1,7 +1,27 @@
 module RunLength (decode, encode) where
 
+import Data.Char
+import Data.List
+
 decode :: String -> String
-decode encodedText = error "You need to implement this function."
+decode [] = ""
+decode encodedText = do
+            let (x,xs)  = span isDigit encodedText
+            if x == []
+            then decodeChar x xs ++ (decode $ tail encodedText)
+            else decodeChar x xs ++ (decode $ tail $ xs )
+
+decodeChar :: String -> String -> String
+decodeChar [] (x:_)  = [x]
+decodeChar n (x:xs) = replicate (read n :: Int) x
+
 
 encode :: String -> String
-encode text = error "You need to implement this function."
+encode [] = ""
+encode text@(x:xs) = do
+           let (same, diff) = span ((==) x) text
+           let r = if (length same) == 1
+                        then same
+                        else (show $ length same ) ++ [head same]
+           r ++ encode diff
+
