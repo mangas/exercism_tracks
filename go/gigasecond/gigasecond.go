0dee85ce492b99c08ac6c1303c9d77638decb917
Filipe Azevedo <filipe@azevedo.io>
2017-12-31 02:33:22 +0000

    style fixes

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -2,9 +2,8 @@ package gigasecond
 
 import "time"
 
-
 var SECS int = 1000000000
 
 func AddGigasecond(t time.Time) time.Time {
-	return t.Add(time.Duration(SECS)*time.Second)
+	return t.Add(time.Duration(SECS) * time.Second)
 }
