744c3f0f109d082efd7c7f44fd47200d1c1dde5f
Filipe Azevedo <filipe@azevedo.io>
2018-01-01 02:48:42 +0000

    go bob exercise

---
+++ go/bob/bob.go
@@ -0,0 +1,54 @@
+package bob
+
+import (
+	"strings"
+	"unicode"
+)
+
+func Hey(remark string) string {
+
+	noSpaces := strings.Join(strings.Fields(remark), "")
+	allLetters := strings.Map(noPunct, noSpaces)
+	noNumbers := strings.TrimSpace(strings.Map(remNumbers, allLetters))
+
+	switch {
+	case len(noSpaces) == 0:
+		return "Fine. Be that way!"
+	case len(allLetters) == 0:
+		if strings.HasSuffix(remark, "?") {
+			return "Sure."
+		} else {
+			return "Whoa, chill out !"
+		}
+	case allLetters == strings.ToUpper(allLetters) && len(noNumbers) != 0:
+		if !strings.HasSuffix(remark, "?") {
+			return "Whoa, chill out!"
+		} else {
+			return "Calm down, I know what I'm doing!"
+		}
+
+	case strings.HasSuffix(noSpaces, "?"):
+		return "Sure."
+	default:
+		return "Whatever."
+	}
+
+}
+
+func remNumbers(r rune) rune {
+	switch {
+	case unicode.IsNumber(r):
+		return -1
+	default:
+		return r
+	}
+}
+
+func noPunct(r rune) rune {
+	switch {
+	case unicode.IsPunct(r):
+		return -1
+	default:
+		return r
+	}
+}
